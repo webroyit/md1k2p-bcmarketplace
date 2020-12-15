be34771eb0acb40ac80dbe31dca882871b2a24ba
royweb <xytank@gmail.com>
2020-12-15 12:41:31 -0500

    Set up contract and react

---
+++ src/component/Navbar.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-dark fixed-top bg-warning flex-md-nowrap p-0 shadow">
+                <a
+                    className="navbar-brand col-sm-3 col-md-2 mr-0 text-dark"
+                    href="/"
+                >
+                Drink Shop
+                </a>
+                <ul className="navbar-nav px-3">
+                <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+                    <small className="text-secondary"><span id="account">0x0</span></small>
+                </li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
