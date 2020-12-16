44753d489a5bfa48b75ad9615731a0dee83ec176
royweb <xytank@gmail.com>
2020-12-16 12:07:31 -0500

    Create a struct for Product

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -6,6 +6,21 @@ contract Marketplace{
     // 'public' will create a function that allows other contract to get this value 
     string public name;
 
+    // To keep track of how many products in the hash map
+    uint public productCount = 0;
+
+    // Store the sturct Product in a hash map or associative array
+    mapping(uint => Product) public products;
+
+    // Add items to the marketplace as a seller
+    struct Product{
+        uint id;
+        string name;
+        uint price;
+        address owner;
+        bool purchased;
+    }
+
     // It get run only once when the contract is deployed
     constructor() public{
         name = "Drink Place";
