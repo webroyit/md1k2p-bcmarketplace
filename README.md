08f36f90d50103cb63636d78596bfdab1edcd7d5
royweb <xytank@gmail.com>
2020-12-18 13:15:43 -0500

    Install web3 and use it to connect to the blockchain

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # BC Marketplace
 
+## Packages
+- `web3` to interact with ethereum node
+
 ## Notes
 - ABI tells how the smart contract works
--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
 import './App.css';
 
 import Navbar from './component/Navbar';
 
 class App extends Component {
+  async componentWillMount(){
+    await this.loadWeb3();
+  }
+
+  async loadWeb3(){
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      // Creates a connection to the blockchain
+      window.web3 = new Web3(window.ethereum);
+
+      // Request account access if needed
+      await window.ethereum.enable();
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider);
+    }
+    // Non-dapp browsers...
+    else {
+        window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+  }
+
   render() {
     return (
       <div className="App">
