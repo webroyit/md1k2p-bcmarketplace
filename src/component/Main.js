86f6d3a61af948ab749ebb779aa2dd166cec6896
royweb <xytank@gmail.com>
2020-12-19 13:06:34 -0500

    Add a form

--- src/component/Main.js
+++ src/component/Main.js
@@ -6,6 +6,27 @@ class Main extends Component {
     return (
         <div id="content">
             <h1>Add product</h1>
+            <form>
+                <div className="form-group mr-sm-2">
+                    <input
+                        id="productName"
+                        type="text"
+                        ref={(input) => {this.productName = input }}
+                        className="form-control"
+                        placeholder="Product Name"
+                        required />
+                </div>
+                <div className="form-group mr-sm-2">
+                    <input
+                        id="productPrice"
+                        type="text"
+                        ref={(input) => {this.productPrice = input }}
+                        className="form-control"
+                        placeholder="Product Price"
+                        required />
+                </div>
+                <button type="submit" className="btn btn-warning">Add Product</button>
+            </form>
         </div>
     );
   }
